4f20c38929129d04660a9bc74756d7f136df18da
NicolaM94 <nicola.moro2312@gmail.com>
2020-11-17 00:49:05 +0100

    Working

--- src/main/kotlin/Classes/CostClass.kt
+++ src/main/kotlin/Classes/CostClass.kt
@@ -2,6 +2,7 @@ package Classes
 
 import javafx.beans.property.SimpleListProperty
 import tornadofx.asObservable
+import java.util.zip.CheckedOutputStream
 import javax.swing.text.MutableAttributeSet
 
 object CostClass  {
@@ -163,7 +164,166 @@ object CostClass  {
         }
         return collector
     }
+    fun intangibleDepreciations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34001.00..34100.0) collector.add(it)
+        }
+        return collector
+    }
+    fun fieldsAndFabricatesDepreciations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34101.00..34110.0) collector.add(it)
+        }
+        return collector
+    }
+    fun industrialDeprectiations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34111.00..34122.99) collector.add(it)
+        }
+        return collector
+    }
+    fun commercialDepreciations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34122.00..34134.99) collector.add(it)
+        }
+        return collector
+    }
+    fun veichlesDepreciations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34135.00..34200.0) collector.add(it)
+        }
+        return collector
+    }
+    fun depreciationsIntangibleAssets () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34201.00..34300.0) collector.add(it)
+        }
+        return collector
+    }
+    fun depreciationsTangibleAssets () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34301.00..34400.0) collector.add(it)
+        }
+        return collector
+    }
+    fun inventoriesDeprecitations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34411.00..34500.0) collector.add(it)
+        }
+        return collector
+    }
+    fun riskFundsProvisions () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 34510.00..35000.0) collector.add(it)
+        }
+        return collector
+    }
+    fun taxesAndRights () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 35001.00..35200.0) collector.add(it)
+        }
+        return collector
+    }
+    fun otherManagerialCosts () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 35201.00..35300.0) collector.add(it)
+        }
+        return collector
+    }
+    fun adjustmentsManagerialCosts () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 35310.00..36000.0) collector.add(it)
+        }
+        return collector
+    }
 
-
-
+    fun startingInventories () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 36011.00..37000.0) collector.add(it)
+        }
+        return collector
+    }
+    fun capitalLossesOfPartecipations () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 37010.00..37100.0) collector.add(it)
+        }
+        return collector
+    }
+    fun passiveInterests () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 37111.00..37200.0) collector.add(it)
+        }
+        return collector
+    }
+    fun otherFinancialCosts () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 37201.00..37300.0) collector.add(it)
+        }
+        return collector
+    }
+    fun depreciationsFinancialActivities () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 37310.00..38000.0) collector.add(it)
+        }
+        return collector
+    }
+    fun extraordinaryCapitalLosses () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 38010.00..38100.0) collector.add(it)
+        }
+        return collector
+    }
+    fun previousAndDeferredTaxes () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 38110.00..38200.0 || it.countId in 39010.00..39200.00) collector.add(it)
+        }
+        return collector
+    }
+    fun otherExtraordinaryCosts () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 38210.00..39000.0) collector.add(it)
+        }
+        return collector
+    }
+    fun revenuesConsolidatedRegime () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 39260.00..39360.99) collector.add(it)
+        }
+        return collector
+    }
+    fun costsConsolidatedRegime () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 39210.00..39310.99) collector.add(it)
+        }
+        return collector
+    }
+    fun profitOrLoss () :MutableList<Count> {
+        val collector = mutableListOf<Count>()
+        allCostCountsProperty.forEach {
+            if (it.countId in 39510.00..39580.99) collector.add(it)
+        }
+        return collector
+    }
 }
